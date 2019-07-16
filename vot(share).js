9ac516b3c8c3b2d80a214715629164466a2becbc
nolbo <52912534+nolbo@users.noreply.github.com>
2019-07-16 18:56:10 +0900

    Update vot(share).js

--- vot(share).js
+++ vot(share).js
@@ -1,4 +1,4 @@
-/*vot 배포버전 1.0.32*/
+/*vot 배포버전 1.0.42*/
 
 /*
 Made by nolbo
@@ -324,6 +324,19 @@ function response(room, msg, sender, isGroupChat, replier, ImageDB, packageName,
                 f = f.replace("3", "decodeURIComponent");
                 replier.reply("\n"+f+"로 변환한 값입니다.\n"+AllView+"\n"+result+"\n");
             }
+        }
+
+        else if(msg == "device"){  //실제 vot에 추가되지 않은 명령어
+            var b = Device.getAndroidVersionCode(); //안드로이드 버전
+            var c = Device.getAndroidVersionName(); //버전코드
+            var e = Device.getPhoneModel();  //기기모델
+            var f = Device.isCharging();  //충전여부
+            var g = Device.getPlugType();  //전원
+            var h = Device.getBatteryLevel();  //배터리 잔량
+            var j = Device.getBatteryTemperature() * 0.1; //배터리 온도 (섭씨)
+            var k = Device.getBatteryVoltage() * 0.001;  //배터리 전압 (볼트)
+
+            replier.reply("\n안드로이드 버전 : "+c+"\n버전코드 : "+b+"\n\n기기 모델 : "+e+"\n충전여부 : "+f+"\n전원 : "+g+"\n배터리 잔량 : "+h+"%\n배터리 온도 : "+j+"°C\n배터리 전압 : "+k+"V\n");
         }
 
     }catch (e){
